48f7864924c7df913732a72560acbc5469d84e73
Liju001 <111391752+Liju001@users.noreply.github.com>
2022-08-28 23:16:05 +0600

    Update Oldclone1.py

--- Oldclone1.py
+++ Oldclone1.py
@@ -13,7 +13,8 @@ psb('\x1b[1;32mBANGLADESH LIJU 11 DIGIT CLONING START PLEASE WAIT ..........')
 for n in range(9999):
     nmbr = random.randint(1111111, 9999999)
     sys.stdout = open('.txt', 'a')
-    print nmbr(...)
+    print nmbr
+    ^^^^^^^^^^
     sys.stdout.flush()
 
 try:
